feef7aa82819bdda46361593acb73962169e95d9
NRxl <nathan.rouxel@gmail.com>
2014-01-18 10:43:50 +0100

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -12,19 +12,12 @@ int main(){
     openWindow(W, H, "The Quake is a lie");
     int endgame = 0; //Entier pour: 0 continue 1 fin du jeu 2 alt pressé 3 f4 pressé
     Image ecran;
-    Player player;
-<<<<<<< HEAD
     Map map("test"); 
+	Player player(map);
     
     while(endgame != 1){
-        handleEvent(endgame, player);
+        handleEvent(endgame, player, map);
         graphics(ecran, player, map);
-=======
-    
-    while(endgame != 1){
-        handleEvent(endgame, player);
-        graphics(ecran, player);
->>>>>>> 5b2f1a49c4c0fa7e550814b3a07524a8fa8fe7ef
         ecran.display();
         milliSleep(30);
     }
