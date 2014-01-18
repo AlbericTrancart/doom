19e86b3485f7e9295e33ec24c68e5d406190a9b3
NRxl <nathan.rouxel@gmail.com>
2014-01-18 10:46:30 +0100

    Update graphics.h

--- cpp/headers/graphics.h
+++ cpp/headers/graphics.h
@@ -3,8 +3,4 @@
 #include "hud.h"
 #include "map.h"
 
-<<<<<<< HEAD
 void graphics(Image& img, Player player, Map& map);
-=======
-void graphics(Image& img, Player player);
->>>>>>> 5b2f1a49c4c0fa7e550814b3a07524a8fa8fe7ef
