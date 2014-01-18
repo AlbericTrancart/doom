2e5d357baf628a7b6f6879ea1147eed0bd2ef6d4
NRxl <nathan.rouxel@gmail.com>
2014-01-18 10:48:45 +0100

    Update utils.h

--- cpp/headers/utils.h
+++ cpp/headers/utils.h
@@ -34,11 +34,12 @@ bool operator!=(Point a, Point b);
 
 //***CLASS EDGE DECLARATIONS***//
 struct Edge {
-    Point A, B;
-    int type;
+    Point A, B; //Bord de l'aeête
+    int type; //Mur ou pas
     
 public:
-    bool sameSide(Point a, Point b); //Vérifie si les points a et b sont dans le même demi plan formé par la droite définie à partir de l'arete
+    bool sameSide(Point a, Point b); //Vérifie si les points a et b sont dans le même demi plan formé par la droite définie à partir 
+									 //de l'arete E1.sameSide(P1,A)
 };
 bool operator==(Edge a, Edge b);
 bool operator!=(Edge a, Edge b);
@@ -46,10 +47,10 @@ bool operator!=(Edge a, Edge b);
 //***CLASS FACE DECLARATIONS***//
 struct Face {
     Edge E1, E2, E3;
-    Point P1, P2, P3; //Le point P1 est opposé à l'arête E1, etc.
+    Point P1, P2, P3; //Le point P1 est opposé à l'arête E1, etc. Sommets de la face
     
 public:
-    bool isInFace(Point p); //Dit si le point est dans la face (wesh si on s'est pris un pain dans la gueule)
+    bool isInFace(Point p); //Dit si le point est dans la face (wesh si on s'est pris un pain dans la gueule) (3 fois sameSide)
     void loadPoints(); //Charge les points correspondants aux arêtes, utilisé lors de l'initialisation de la map
 };
 bool operator==(Face a, Face b);
@@ -63,11 +64,11 @@ public:
     int w, h;
     
     Image();
-    Image(string src);
+    Image(string src); //Image I("/.../img.png");
     ~Image();
     
     Color get(int x, int y); //Avoir un pixel
     void set(int x, int y, Color value); //Donner une valeur à un pixel
     void display(); //Afficher l'image
-    void addSprite(Image sprite, int x, int y); //Place une image en x, y en tenant compte de la transparence
+    void addSprite(Image& sprite, int x, int y); //Place une image en x, y en tenant compte de la transparence
 };
