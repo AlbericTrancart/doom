a072e2873ff62df0343b898e5b85db64c4bda27f
NRxl <nathan.rouxel@gmail.com>
2014-01-18 10:44:28 +0100

    Update map.cpp

--- cpp/map.cpp
+++ cpp/map.cpp
@@ -1,37 +1,38 @@
 #include "headers/map.h"
 
-<<<<<<< HEAD
 //Utilitaire pour la lecture du fichier: prend une ligne et renvoie dans un array tous les entiers de la ligne dans l'ordre
-//L'index 0 est la position du dernier élément et la recherche commence à parti de la position 1 de la chaine.
+//L'index 0 est la position du dernier élément et la recherche commence à partir de la position 1 de la chaine string.
 //Les curseurs prennent comme valeur la position des espaces.
-void getInts(string line, int result[7]){
+void getInts(string line, int result[7]){ //Prend la première ligne du test.map et la met dans un string nommé line
     int curs1 = 1, curs2;
-    int i = 1; //Compteur: nb d'entiers rangés dans le tableau
+    int i = 1; //Compteurs: i est le numéro de l'entier trouvé
     
-    while(line.find_first_of(' ', curs1+1) != string::npos){
+    while(line.find_first_of(' ', curs1+1) != string::npos){ //Cherche le premier espace à partir de curs1=2
+															 //npos valeur renvoyée si fin de la chaîne atteinte sans trouver d'espace
         curs2 = line.find_first_of(' ', curs1+1);
         result[i] = atoi(line.substr(curs1+1, curs2-curs1-1).c_str()); //On sélectionne ce qu'il y a entre curs1 et curs2 et on convertit en int
-        i++;
+																	   //atoi convertit le tableau de char en int. c_str() convertit string en tableau de char chaque case du tableau =1 caractère. line_substr renvoie le string (de tel caractère, sur telle longueur).
+        ++i;
         curs1 = curs2;
     }
     //On rajoute la fin de la ligne
     result[i] = atoi(line.substr(curs1+1).c_str());
-    i++;
+    ++i;
     result[0] = i;
 }
 
 //***CLASS MAP***//
 //--------------------------------------------------------------------//
 
-Map::Map(string src){  
-    ifstream fichier(("maps/"+src+".map").c_str(), ios::in);
+Map::Map(string src){ //Map map("test");
+    ifstream fichier(("maps/"+src+".map").c_str(), ios::in); //ifstream ouvre un fichier en lecture. ios::in méthode d'ouverture
     assert(fichier);
     
     string line;
     int result[7];
-
-    getline(fichier, line); //Saute une ligne
-    getline(fichier, line);
+    
+    getline(fichier, line); //met dans "line" la ligne sous la forme d'un string puis saute une ligne (curseur virtuel)
+    getline(fichier, line); //(line est modifié -passé par référence-) à cette ligne propriétés générales de la map
     getInts(line, result);
     w = result[1];
     h = result[2];
@@ -48,8 +49,8 @@ Map::Map(string src){
         switch(line[0]){
             case 'P':
                 getInts(line, result);
-                pnt[result[1]].x = result[1];
-                pnt[result[1]].y = result[2];
+                pnt[result[1]].x = result[2];
+                pnt[result[1]].y = result[3];
                 break;
                 
             case 'A':
@@ -116,33 +117,14 @@ float Map::findWall(Player player, float angle){
             (X = b*(x2-x1)+x1
             (Y = b*(y2-y1)+y1
             Maintenant vous avez foi dans mes compétences en géométrie*/
-            int xp = player.pos.x, yp = player.pos.y, x1 = e.A.x, y1 = e.A.y, x2 = e.B.x, y2 = e.B.y;
-            int x = -(cos(angle)*(-x1*y2+y1*x2+yp*(x1-x2))+sin(angle)*xp*(x2-x1))/(cos(angle)*(y2-y1)+sin(angle)*(x1-x2));
-            int y = -(sin(angle)*(-x1*y2+y1*x2+xp*(y2-y1))+cos(angle)*yp*(y1-y2))/(cos(angle)*(y2-y1)+sin(angle)*(x1-x2));
+            float xp = player.pos.x, yp = player.pos.y, x1 = e.A.x, y1 = e.A.y, x2 = e.B.x, y2 = e.B.y;
+            float x = -(cos(angle)*(-x1*y2+y1*x2+yp*(x1-x2))+sin(angle)*xp*(x2-x1))/(cos(angle)*(y2-y1)+sin(angle)*(x1-x2));
+            float y = -(sin(angle)*(-x1*y2+y1*x2+xp*(y2-y1))+cos(angle)*yp*(y1-y2))/(cos(angle)*(y2-y1)+sin(angle)*(x1-x2));
             //On peut le faire en utilisant le point à l'infini mais l'expression est encore plus illisible
             dist = sqrt((xp-x)*(xp-x)+(yp-y)*(yp-y));
         }
     } while(!bang_le_mur);
     
-    return dist;
+    return 5;
 }
 //--------------------------------------------------------------------//
-=======
-bool isInFace(Player p){
-    return true;
-}
-
-float findWall(Player p){
-    float dist; //Distance au mur
-    
-    //On lance le rayon et on passe de face en face jusqu'à ce que l'on se cogne contre un mur
-    bool bang_le_mur = true;
-    do{
-        bang_le_mur = false;
-    } while(bang_le_mur);
-    
-    dist = 10;
-    
-    return dist;
-}
->>>>>>> 5b2f1a49c4c0fa7e550814b3a07524a8fa8fe7ef
