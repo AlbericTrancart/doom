848dcf0bf8d0d8f9f4c9ce3c0cd391f5f752af69
NRxl <nathan.rouxel@gmail.com>
2014-01-18 10:46:53 +0100

    Update map.h

--- cpp/headers/map.h
+++ cpp/headers/map.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "player.h"
 
-<<<<<<< HEAD
 //***Structure du fichier stockant la map***//
 /*M Largeur hauteur nbpoints nbaretes nbfaces
 Pour les points:
@@ -14,35 +13,18 @@ F id arete1 arete2 arete3
 Il faut ABSOLUMENT garder la structure:
 Infos générales en ligne 2 PUIS les points PUIS les aretes PUIS les faces*/
 
+class Player;
+
 class Map {
+
+public:
     Point* pnt;
     Edge* edg;
     Face* fac;
     int nbPoint, nbEdge, nbFace;
     int w, h;
     
-public:
     Map(string src);
     ~Map();
-    float findWall(Player player, float angle); //Donne la distance au mur le plus proche
+    float findWall(Player player, float angle); //Donne la distance au mur le plus proche map1.findWall(player1,angle)
 };
-=======
-struct Arrete {
-    Point A, B;
-};
-
-struct Face {
-    Point A, B, C;
-};
-
-class Map {
-    Arrete* arr;
-    Face* faces;
-    
-public:
-    bool isInFace(Point p);
-};
-
- //Dit si le point est dans la face (wesh si on s'est pris un pain dans la gueule)
-float findWall(Player p); //Donne la distance au mur le plus proche
->>>>>>> 5b2f1a49c4c0fa7e550814b3a07524a8fa8fe7ef
