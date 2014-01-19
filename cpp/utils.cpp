d0345549287186a62e19423a0a56087958563274
NRxl <nathan.rouxel@gmail.com>
2014-01-19 13:56:30 +0100

    Update utils.cpp

--- cpp/utils.cpp
+++ cpp/utils.cpp
@@ -16,7 +16,7 @@ Point::Point() //constructeur vide
 
 
 //--------------------------------------------------------------------//
-Point Point::operator+(Point Q) //opérateur +
+Point Point::operator+(Point Q) //opï¿½rateur +
 {
 	Point S;
 	S.x=x+Q.x;
@@ -29,7 +29,7 @@ float Point::operator*(Point Q) //produit scalaire
 	return x*Q.x+y*Q.y;
 }
 
-Point Point::operator-(Point Q) //opérateur -
+Point Point::operator-(Point Q) //opï¿½rateur -
 {
 	Point S;
 	S.x=x-Q.x;
@@ -76,8 +76,8 @@ bool operator!=(Point a, Point b)
 //***CLASS EDGE***//
 //--------------------------------------------------------------------//
 bool Edge::sameSide(Point a, Point b){
-    //Soit la droite formée par les deux points extremes de l'arête
-    //On détermine l'équation de la droite pour savoir si les points sont "en dessous" ou "au dessus"
+    //Soit la droite formï¿½e par les deux points extremes de l'arï¿½te
+    //On dï¿½termine l'ï¿½quation de la droite pour savoir si les points sont "en dessous" ou "au dessus"
     bool test1, test2;
     if(A.x != B.x){
         test1 = (a.y <= (a.x-A.x)*(B.y-A.y)/(B.x-A.x)+A.y);
@@ -87,7 +87,7 @@ bool Edge::sameSide(Point a, Point b){
         test1 = (a.x <= A.x);
         test2 = (b.x <= A.x);
     }
-    return !(test1 xor test2);
+    return !(test1 ^ test2);
 }
 
 bool operator==(Edge a, Edge b)
@@ -107,16 +107,16 @@ bool Face::isInFace(Point p){
 }
 
 void Face::loadPoints(){
-    //On va chercher les arêtes et les points correspondants et on les ordonne
-    Point p2 = E1.A, p3 = E1.B; //p2 et p3 appartiennent à E1
-    if(p3 == E2.A || p3 == E2.B) //Si p3 appartient à E2
-        P3 = p3; //Alors p3 est opposé à E3
-    else //Sinon p3 n'appartient pas à E2 donc est opposé à E2
+    //On va chercher les arï¿½tes et les points correspondants et on les ordonne
+    Point p2 = E1.A, p3 = E1.B; //p2 et p3 appartiennent ï¿½ E1
+    if(p3 == E2.A || p3 == E2.B) //Si p3 appartient ï¿½ E2
+        P3 = p3; //Alors p3 est opposï¿½ ï¿½ E3
+    else //Sinon p3 n'appartient pas ï¿½ E2 donc est opposï¿½ ï¿½ E2
         P2 = p3;
     
-    if(p2 == E3.A || p2 == E3.B) //Si p2 appartient à E3
-        P2 = p2; //Alors p2 est opposé à E2
-    else //Sinon p2 n'appartient pas à E3 donc est opposé à E3
+    if(p2 == E3.A || p2 == E3.B) //Si p2 appartient ï¿½ E3
+        P2 = p2; //Alors p2 est opposï¿½ ï¿½ E2
+    else //Sinon p2 n'appartient pas ï¿½ E3 donc est opposï¿½ ï¿½ E3
         P3 = p2;
     
     //P1 est donc le dernier point
@@ -129,7 +129,7 @@ void Face::loadPoints(){
 
 bool operator==(Face a, Face b)
 {
-    return (a.E1 == b.E1 && a.E2 == b.E2); //Il suffit qu'il y ait deux arêtes en commun pour avoir l'égalité
+    return (a.E1 == b.E1 && a.E2 == b.E2); //Il suffit qu'il y ait deux arï¿½tes en commun pour avoir l'ï¿½galitï¿½
 }
 
 bool operator!=(Face a, Face b)
@@ -147,7 +147,7 @@ Image::Image(){
         cols[i] = WHITE;
 }
 
-//Charge les images à partir de fichiers
+//Charge les images ï¿½ partir de fichiers
 Image::Image(string src){
     loadColorImage(src, cols, w, h);
 }
@@ -162,7 +162,7 @@ Color Image::get(int x, int y){
     return cols[y + x*W];
 }
 
-//Donner une valeur à un pixel
+//Donner une valeur ï¿½ un pixel
 void Image::set(int x, int y, Color value){
     cols[y + x*W] = value;
 }
@@ -172,7 +172,7 @@ void Image::display(){
     putColorImage(0, 0, cols, w, h);
 }
 
-void Image::addSprite(Image& sprite, int x, int y){ //ecran.addSprite(sprite,x,y) points (x,y) point en haut à gauche du rectangle
+void Image::addSprite(Image& sprite, int x, int y){ //ecran.addSprite(sprite,x,y) points (x,y) point en haut ï¿½ gauche du rectangle
     for(int i = y; i < min(y+sprite.h, H); ++i)
         for(int j = x; j < min(x+sprite.w, W); ++j)
             if(sprite.get(i,j) != WHITE)
