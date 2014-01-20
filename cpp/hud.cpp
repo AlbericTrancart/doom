c9942b61413c9c3c84a2b47b3a379d25d64f4622
AlbericTrancart <alberic.trancart@eleves.enpc.fr>
2014-01-20 20:18:24 +0100

    Update map.cpp

--- cpp/hud.cpp
+++ cpp/hud.cpp
@@ -10,7 +10,7 @@ Image Wsg1("shotgun/1.png"), Wsg2("shotgun/2.png"), Wsg3("shotgun/3.png"), Wsg4(
 void weapon(Image& img, Player player){
  
     //Pour chaque arme on définit une table avec les différentes images
-    if(player.weapon == 1){
+    if(player.weapon == 1){ //Poings
         if(player.weapon_state == 0)
             img.addSprite(Wf1, W/2-300, H-150);
         else if(player.weapon_state > 0 && player.weapon_state < 5)
@@ -18,53 +18,63 @@ void weapon(Image& img, Player player){
         else if(player.weapon_state >= 5 && player.weapon_state < 8)
             img.addSprite(Wf3, W/2-300, H-150);
     }
-    if(player.weapon == 2){
+    if(player.weapon == 2){ //Gun
         if(player.weapon_state == 0)
             img.addSprite(Wg1, W/2-300, H-150);
         else if(player.weapon_state > 0 && player.weapon_state < 5)
             img.addSprite(Wg2, W/2-300, H-150);
-        else if(player.weapon_state >= 5 && player.weapon_state < 8)
+        else if(player.weapon_state >= 5 && player.weapon_state < 6)
             img.addSprite(Wg3, W/2-300, H-150);
-        else if(player.weapon_state >= 8 && player.weapon_state < 12)
+        else if(player.weapon_state >= 6 && player.weapon_state < 7)
             img.addSprite(Wg4, W/2-300, H-150);
     }
-    if(player.weapon == 3){
+    if(player.weapon == 3){ //Shotgun
         if(player.weapon_state == 0)
             img.addSprite(Wsg1, W/2-300, H-150);
-        else if(player.weapon_state > 0 && player.weapon_state < 5)
+        else if(player.weapon_state > 0 && player.weapon_state < 4)
             img.addSprite(Wsg2, W/2-300, H-150);
-        else if(player.weapon_state >= 5 && player.weapon_state < 8)
+        else if(player.weapon_state >= 4 && player.weapon_state < 6)
             img.addSprite(Wsg3, W/2-300, H-150);
-        else if(player.weapon_state >= 8 && player.weapon_state < 11)
+        else if(player.weapon_state >= 6 && player.weapon_state < 9)
             img.addSprite(Wsg4, W/2-300, H-150);
-        else if(player.weapon_state >= 11 && player.weapon_state < 14)
+        else if(player.weapon_state >= 9 && player.weapon_state < 11)
             img.addSprite(Wsg5, W/2-300, H-150);
-        else if(player.weapon_state >= 14 && player.weapon_state < 18)
+        else if(player.weapon_state >= 11 && player.weapon_state < 14)
             img.addSprite(Wsg6, W/2-300, H-150);
     }
-    if(player.weapon == 4){
+    if(player.weapon == 4){ //Carbine
         if(player.weapon_state == 0)
             img.addSprite(Wc1, W/2-300, H-150);
-        else if(player.weapon_state > 0 && player.weapon_state < 5)
+        else if(player.weapon_state > 0 && player.weapon_state < 4)
             img.addSprite(Wc2, W/2-300, H-150);
-        else if(player.weapon_state >= 5 && player.weapon_state < 8)
+        else if(player.weapon_state >= 4 && player.weapon_state < 6)
             img.addSprite(Wc3, W/2-300, H-150);
+        else if(player.weapon_state >= 6 && player.weapon_state < 10)
+            img.addSprite(Wc4, W/2-300, H-150);
+        else if(player.weapon_state >= 10 && player.weapon_state < 13)
+            img.addSprite(Wc5, W/2-300, H-150);
+        else if(player.weapon_state >= 13 && player.weapon_state < 15)
+            img.addSprite(Wc6, W/2-300, H-150);
     }
-    if(player.weapon == 5){
-        if(player.weapon_state == 0)
+    if(player.weapon == 5){ //Machine gun
+        if(player.weapon_state < 10)
             img.addSprite(Wmg1, W/2-300, H-150);
-        else if(player.weapon_state > 0 && player.weapon_state < 5)
+        else if(player.weapon_state >= 10 && player.weapon_state < 11)
             img.addSprite(Wmg2, W/2-300, H-150);
-        else if(player.weapon_state >= 5 && player.weapon_state < 8)
+        else if(player.weapon_state >= 11 && player.weapon_state < 12)
             img.addSprite(Wmg3, W/2-300, H-150);
+        else if(player.weapon_state >= 12 && player.weapon_state < 13)
+            img.addSprite(Wmg4, W/2-300, H-150);
     }
-    if(player.weapon == 6){
+    if(player.weapon == 6){ //Plasma gun
         if(player.weapon_state == 0)
             img.addSprite(Wpg1, W/2-300, H-150);
-        else if(player.weapon_state > 0 && player.weapon_state < 5)
+        else if(player.weapon_state > 0 && player.weapon_state < 8)
             img.addSprite(Wpg2, W/2-300, H-150);
-        else if(player.weapon_state >= 5 && player.weapon_state < 8)
+        else if(player.weapon_state >= 8 && player.weapon_state < 13)
             img.addSprite(Wpg3, W/2-300, H-150);
+        else if(player.weapon_state >= 13 && player.weapon_state < 15)
+            img.addSprite(Wpg4, W/2-300, H-150);
     }
 }
 
--- cpp/map.cpp
+++ cpp/map.cpp
@@ -1,17 +1,17 @@
 #include "headers/map.h"
 
-//Utilitaire pour la lecture du fichier: prend une ligne et renvoie dans un array tous les entiers de la ligne dans l'ordre
+//Utilitaire pour la lecture du fichier: prend une ligne et renvoie dans un array tous les nombres de la ligne dans l'ordre
 //L'index 0 est la position du dernier élément et la recherche commence à partir de la position 1 de la chaine string.
 //Les curseurs prennent comme valeur la position des espaces.
-void getInts(string line, int result[7]){ //Prend la première ligne du test.map et la met dans un string nommé line
+void getData(string line, float result[7]){ //Prend la première ligne du fichier map et la met dans un string nommé line
     int curs1 = 1, curs2;
-    int i = 1; //Compteurs: i est le numéro de l'entier trouvé
+    int i = 1; //Compteurs: i est le numéro du nombre trouvé
     
     while(line.find_first_of(' ', curs1+1) != string::npos){ //Cherche le premier espace à partir de curs1=2
 															 //npos valeur renvoyée si fin de la chaîne atteinte sans trouver d'espace
         curs2 = line.find_first_of(' ', curs1+1);
-        result[i] = atoi(line.substr(curs1+1, curs2-curs1-1).c_str()); //On sélectionne ce qu'il y a entre curs1 et curs2 et on convertit en int
-																	   //atoi convertit le tableau de char en int. c_str() convertit string en tableau de char chaque case du tableau =1 caractère. line_substr renvoie le string (de tel caractère, sur telle longueur).
+        result[i] = atof(line.substr(curs1+1, curs2-curs1-1).c_str()); //On sélectionne ce qu'il y a entre curs1 et curs2 et on convertit en int
+																	   //atof convertit le tableau de char en float. c_str() convertit string en tableau de char chaque case du tableau =1 caractère. line_substr renvoie le string (de tel caractère, sur telle longueur).
         ++i;
         curs1 = curs2;
     }
@@ -29,16 +29,16 @@ Map::Map(string src){ //Map map("test");
     assert(fichier);
     
     string line;
-    int result[7];
+    float result[7];
     
     getline(fichier, line); //met dans "line" la ligne sous la forme d'un string puis saute une ligne (curseur virtuel)
     getline(fichier, line); //(line est modifié -passé par référence-) à cette ligne propriétés générales de la map
-    getInts(line, result);
-    w = result[1];
-    h = result[2];
-    nbPoint = result[3];
-    nbEdge = result[4];
-    nbFace = result[5];
+    getData(line, result);
+    w = int(result[1]);
+    h = int(result[2]);
+    nbPoint = int(result[3]);
+    nbEdge = int(result[4]);
+    nbFace = int(result[5]);
     cout << "Chargement d'une map de " << w << "x" << h << " comportant " << nbPoint << " points, " << nbEdge << " aretes et " << nbFace << " faces..." << endl;
 
     pnt = new Point[nbPoint];
@@ -48,32 +48,32 @@ Map::Map(string src){ //Map map("test");
    do{
         switch(line[0]){
             case 'P':
-                getInts(line, result);
-                pnt[result[1]].x = result[2];
-                pnt[result[1]].y = result[3];
+                getData(line, result);
+                pnt[int(result[1])].x = result[2];
+                pnt[int(result[1])].y = result[3];
                 break;
                 
             case 'A':
-                getInts(line, result);
-                edg[result[1]].A = pnt[result[2]];
-                edg[result[1]].B = pnt[result[3]];
-                edg[result[1]].type = result[4];
-                if(!(result[5] >= 0 && result[5] < nbFace))
-                    edg[result[1]].F1 = -1;
+                getData(line, result);
+                edg[int(result[1])].A = pnt[int(result[2])];
+                edg[int(result[1])].B = pnt[int(result[3])];
+                edg[int(result[1])].type = int(result[4]);
+                if(!(int(result[5]) >= 0 && int(result[5]) < nbFace))
+                    edg[int(result[1])].F1 = -1;
                 else
-                    edg[result[1]].F1 = result[5];
-                if(!(result[6] >= 0 && result[6] < nbFace))
-                    edg[result[1]].F2 = -1;
+                    edg[int(result[1])].F1 = int(result[5]);
+                if(!(int(result[6]) >= 0 && int(result[6]) < nbFace))
+                    edg[int(result[1])].F2 = -1;
                 else
-                    edg[result[1]].F2 = result[6];
+                    edg[int(result[1])].F2 = int(result[6]);
                 break;
                 
             case 'F':
-                getInts(line, result);
-                fac[result[1]].E1 = edg[result[2]];
-                fac[result[1]].E2 = edg[result[3]];
-                fac[result[1]].E3 = edg[result[4]];
-                fac[result[1]].loadPoints();
+                getData(line, result);
+                fac[int(result[1])].E1 = edg[int(result[2])];
+                fac[int(result[1])].E2 = edg[int(result[3])];
+                fac[int(result[1])].E3 = edg[int(result[4])];
+                fac[int(result[1])].loadPoints();
                 break;
         }
     } while(getline(fichier, line));
