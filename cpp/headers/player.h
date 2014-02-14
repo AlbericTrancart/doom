d7328e4dbf4abcedb5a4db1b997144f998fd817f
NRxl <nathan.rouxel@gmail.com>
2014-02-14 23:44:15 +0100

    Motion

--- cpp/headers/player.h
+++ cpp/headers/player.h
@@ -16,14 +16,16 @@ public:
 
     Player(Map& map); //Construit le joueur dans la map indiquée
 
-    void test_and_move(Point nextpos, Map& map); //Teste si le déplacement est possible dans la map, et l'effectue si c'est le cas
-    void move_up(Map& map, double a); //Décrémente la position dans le sens des y décroissants dans la map de a*PAS
-    void move_down(Map& map, double a); //Incrémente la position dans le sens des y croissants de a*PAS
-    void move_left(Map& map, double a); //Décrémente la position dans le sens des x décroissants de a*PAS
-    void move_right(Map& map, double a); //Incrémente la position dans le sens des x croissants de a*PAS
+    void test_and_move(Point nextpos, Map& map); //Teste si le déplacement est possible dans la map, et l'effectue si c'est le cas (si pas d'argument, a=1)
+    void move_up(Map& map, double a=1); //Décrémente la position dans le sens des y décroissants dans la map de a*PAS (si pas d'argument, a=1)
+    void move_down(Map& map, double a=1); //Incrémente la position dans le sens des y croissants de a*PAS (si pas d'argument, a=1)
+    void move_left(Map& map, double a=1); //Décrémente la position dans le sens des x décroissants de a*PAS (si pas d'argument, a=1)
+    void move_right(Map& map, double a=1); //Incrémente la position dans le sens des x croissants de a*PAS (si pas d'argument, a=1)
 
-    void turn_left(); //Décrémente l'orientation d'un angle THETA
-    void turn_right(); //Incrémente l'orientation d'un angle THETA
+    void turn_left(double a=1); //Décrémente l'orientation d'un angle a*THETA (si pas d'argument, a=1)
+    void turn_right(double a=1); //Incrémente l'orientation d'un angle a*THETA (si pas d'argument, a=1)
+
+	void motion(double e0, double e1);
 
     int what_face(Map& map); //Retourne le numéro de la face dans laquelle se trouve le joueur
 };
